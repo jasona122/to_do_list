e479799db04c9a02e74b9234d24da0df76e40797
Jason A <17jgunawan@gmail.com>
2021-01-14 02:19:50 +0700

    Added alert for adding tasks with no name; modified date display to be clearer

--- listItem.js
+++ listItem.js
@@ -6,10 +6,25 @@ class ListItem {
     }
 
     toDisplay() {
+        let dateString = "-";
+        if(this._date) {
+            // If not empty, format into proper date
+            let date = new Date(this._date);
+            let displayOptions = {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                hour: "numeric",
+                minute: "numeric" 
+            }
+            dateString = date.toLocaleDateString("default", displayOptions);
+        }
+
         let displayData = {
             name: this._name,
             description: this._description,
-            date: this._date
+            date: dateString
         }
         return displayData;
     }
